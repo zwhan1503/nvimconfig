ec58b15dc783fb07c5562665804323a3b129bd46
zwhan <hanthaiduong_hsgs2022@hus.edu.vn>
2025-12-13 01:54:34 +0700

    Adding popup windows theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,11 +4,31 @@ return {
 	priority = 1000,
 	opts = {
 		transparent_background = true,
-		integrations = { telescope = true, bufferline = true },
+		integrations = {
+			telescope = true,
+			bufferline = true,
+			nui = true, -- pop ups configure
+		},
+		custom_highlights = function(colors)
+			return {
+				-- NUI (which competitest uses for popups)
+				NormalFloat = { bg = "none" },
+				FloatBorder = { fg = colors.blue, bg = "none" },
+				FloatTitle = { fg = colors.lavender, bg = "none", style = { "bold" } },
+
+				-- Ensure all popup-related highlights are transparent
+				Pmenu = { bg = "none" },
+				PmenuBorder = { fg = colors.blue, bg = "none" },
+
+				-- Override any potential background highlights
+				["@markup.raw"] = { bg = "none" },
+				["@text.literal"] = { bg = "none" },
+			}
+		end,
 	},
 	config = function(_, opts)
 		require("catppuccin").setup(opts)
 		transparent_background = true
-        vim.cmd.colorscheme("catppuccin")
+		vim.cmd.colorscheme("catppuccin")
 	end,
 }
--- lua/plugins/competitest.lua
+++ lua/plugins/competitest.lua
@@ -1,52 +1,35 @@
 return {
-    "xeluxee/competitest.nvim",
-    dependencies = "MunifTanjim/nui.nvim",
-    ft = { "cpp", "c", "java", "rust" }, -- only load for certain filetypes
-    config = function()
-        require("competitest").setup({
-            compile_command = {
-                cpp = { exec = "g++", args = { "-std=gnu++17", "-O2", "-Wall", "$(FNAME)", "-o", "$(FNOEXT)" } },
-                c = { exec = "gcc", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT)" } },
-                rust = { exec = "rustc", args = { "$(FNAME)" } },
-                java = { exec = "javac", args = { "$(FNAME)" } },
-            },
-            runner_ui = {
-                interface = "split",
-                show_nu = true,
-                viewer = {
-                    width = 0.5,
-                    height = 0.5,
-                    show_nu = true,
-                    show_rnu = false,
-                    open_when_compilation_fails = true,
-                },
-            },
-            split_ui = {
-                position = "right",
-                relative_to_editor = true,
-                total_width = 0.3,
-                vertical_layout = {
-                    { 1, "tc" },
-                    { 1, { { 1, "so" }, { 1, "eo" } } },
-                    { 1, { { 1, "si" }, { 1, "se" } } },
-                },
-                total_height = 0.4,
-                horizontal_layout = {
-                    { 2, "tc" },
-                    { 3, { { 1, "so" }, { 1, "si" } } },
-                    { 3, { { 1, "eo" }, { 1, "se" } } },
-                },
-            },
-            save_current_file = true,
-            save_all_files = false,
-            view_output_diff = true,
-        })
+	"xeluxee/competitest.nvim",
+	dependencies = "MunifTanjim/nui.nvim",
+	ft = { "cpp", "c", "java", "rust" }, -- only load for certain filetypes
+	config = function()
+		require("competitest").setup({
+			compile_command = {
+				cpp = { exec = "g++", args = { "-std=gnu++17", "-O2", "-Wall", "$(FNAME)", "-o", "$(FNOEXT)" } },
+				c = { exec = "gcc", args = { "-Wall", "$(FNAME)", "-o", "$(FNOEXT)" } },
+				rust = { exec = "rustc", args = { "$(FNAME)" } },
+				java = { exec = "javac", args = { "$(FNAME)" } },
+			},
+			popup_ui = {
+				total_width = 0.8,
+				total_height = 0.8,
+				layout = {
+					{ 2, "tc" },
+					{ 3, { { 1, "so" }, { 1, "si" } } },
+					{ 3, { { 1, "eo" }, { 1, "se" } } },
+				},
+			},
+			save_current_file = true,
+			save_all_files = false,
+			view_output_diff = false,
+		})
 
-        -- Keymaps for competitive programming
-        vim.keymap.set("n", "<leader>cr", "<CMD>CompetiTest run<CR>")
-        vim.keymap.set("n", "<leader>cc", "<CMD>CompetiTest receive problem<CR>")
-        vim.keymap.set("n", "<leader>ca", "<CMD>CompetiTest add_testcase<CR>")
-        vim.keymap.set("n", "<leader>cd", "<CMD>CompetiTest delete_testcase<CR>")
-        vim.keymap.set("n", "<leader>ce", "<CMD>CompetiTest edit_testcase<CR>")
-    end,
+		-- Keymaps for competitive programming
+		vim.keymap.set("n", "<leader>cr", "<CMD>CompetiTest run<CR>")
+		vim.keymap.set("n", "<leader>cc", "<CMD>CompetiTest receive problem<CR>")
+		vim.keymap.set("n", "<leader>ca", "<CMD>CompetiTest add_testcase<CR>")
+		vim.keymap.set("n", "<leader>cd", "<CMD>CompetiTest delete_testcase<CR>")
+		vim.keymap.set("n", "<leader>ce", "<CMD>CompetiTest edit_testcase<CR>")
+		vim.keymap.set("n", "<leader>cs", "<CMD>CompetiTest show_ui<CR>")
+	end,
 }
