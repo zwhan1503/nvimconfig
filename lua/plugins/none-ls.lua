5e78fd3dd2caad7fb2853390569de5af60c73291
zwhan <hanthaiduong_hsgs2022@hus.edu.vn>
2026-02-03 23:35:01 +0700

    added formatter for python

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,6 +8,11 @@ return {
 
 		null_ls.setup({
 			sources = {
+				null_ls.builtins.formatting.black.with({
+					extra_args = {
+						"--fast",
+					},
+				}),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.clang_format.with({
