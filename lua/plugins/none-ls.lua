714a421f62c8b789ff3ada823b029c10edcf0a84
zwhan <hanthaiduong_hsgs2022@hus.edu.vn>
2026-02-25 11:48:58 +0700

    changed indent for java to 4

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -13,7 +13,11 @@ return {
 						"--fast",
 					},
 				}),
-                null_ls.builtins.formatting.google_java_format,
+				null_ls.builtins.formatting.google_java_format.with({
+					extra_args = {
+						"--style={IndentWidth: 4}",
+					},
+				}),
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.clang_format.with({
